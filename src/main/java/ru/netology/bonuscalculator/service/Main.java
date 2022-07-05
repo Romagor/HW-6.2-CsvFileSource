ebe5647b0e4b0c2c1e7c9e954d8b6f5f4dcbcb8d
RomaGor <goryachevskii60@gmail.com>
2022-07-05 09:09:36 +0300

    add BonusServiceTest

---
+++ src/main/java/ru/netology/bonuscalculator/service/Main.java
@@ -0,0 +1,14 @@
+package ru.netology.bonuscalculator.service;
+
+public class Main {
+  public static void main(String[] args) {
+    BonusService service = new BonusService();
+
+    long amount = 100060;
+    boolean registered = true;
+
+    long bonus = service.calculate(amount, registered);
+    System.out.println(bonus);
+  }
+}
+
---
+++ src/test/java/ru/netology/bonuscalculator/service/BonusServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.bonuscalculator.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class BonusServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+
+    public void searchExact(long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
